1482b73d0031494838c30b24af656ffee82d36e6
muhammad daqqa <dacca90@gmail.com>
2015-11-10 11:20:12 +0200

    StopWatch Exercise

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MoshSection2Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new StopWatch();
+
+            // stopwatch.Start(); //Just for testing the code
+            stopwatch.Start();
+            Thread.Sleep(3000);
+            stopwatch.Stop();
+
+            var seconds = stopwatch.GetDuration().TotalSeconds;
+            Console.WriteLine(seconds);
+
+            stopwatch.Start();
+            Thread.Sleep(80);
+            stopwatch.Stop();
+
+            seconds = stopwatch.GetDuration().TotalSeconds;
+            Console.WriteLine(seconds);
+        }
+    }
+}
---
+++ StopWatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoshSection2Exercises
+{
+    class StopWatch
+    {
+        private DateTime _startTime;
+        private TimeSpan _timeSpan;
+        private bool _isRunning = false;
+
+        public void Start()
+        {
+            if (this._isRunning == false)
+            {
+                this._startTime = DateTime.Now;
+                this._isRunning = true;
+            }
+            else
+                throw new InvalidOperationException("StopWatch is already running!!");
+        }
+
+        public void Stop()
+        {
+            var timeSpan = DateTime.Now;
+            this._timeSpan = timeSpan - this._startTime;
+            this._isRunning = false;
+        }
+
+        public TimeSpan GetDuration()
+        {
+            return this._timeSpan;
+        }
+       
+    }
+}
